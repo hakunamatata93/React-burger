3adbcddf915bde60b359d70879c47e48e0b698f5
hakunamatata93 <hakuna_matata93@mail.ru>
2022-07-06 15:57:39 +0300

    tabs

--- react-burger/src/components/App/App.js
+++ react-burger/src/components/App/App.js
@@ -1,17 +1,19 @@
-import React from "react";
-
-import AppHeader from "../app-header/app-header";
 import BurgerIngridients from "../burger-ingridients/burger-ingridients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
+import AppHeader from "../app-header/app-header";
+
+import appStyles from './app.module.css';
 
 const App = () => {
   return(
     <>
       <AppHeader />
-      <BurgerIngridients />
-      <BurgerConstructor />
+      <main className={appStyles.main}>
+        <BurgerIngridients />
+        <BurgerConstructor />
+      </main>
     </>
   );
 }
 
-export default App;
+export default App; 
