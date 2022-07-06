cfa3c00c977f09c1241a8fb28fee33bcaac37380
hakunamatata93 <hakuna_matata93@mail.ru>
2022-07-06 22:28:45 +0300

    constructor data

--- react-burger/src/components/burger-constructor/burger-constructor.js
+++ react-burger/src/components/burger-constructor/burger-constructor.js
@@ -1,47 +1,32 @@
 import React from "react";
 
 import { ConstructorElement, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import bun02 from '../../images/bun-02.png';
+import { data } from '../../utils/data.js';
 import icon from '../../images/burger-constructor_icon.svg';
 import burgerConstructorStyles from './burger-constructor.module.css';
+
+const ConstructorItem = ({ cardData }) => {
+  const { image, price, name } = cardData;
+  return(
+    <li className={burgerConstructorStyles.item}>
+        <img src={icon} alt='иконка меню' className={`${burgerConstructorStyles.icon} mr-2`}/>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text={name}
+          price={price}
+          thumbnail={image}
+        />
+      </li>
+  );
+}
 
 const ConstructorItems = () => {
   return (
     <ul className={burgerConstructorStyles.items}>
-      <li className={burgerConstructorStyles.item}>
-        <img src={icon} alt='иконка меню' className={`${burgerConstructorStyles.icon} mr-2`}/>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          //thumbnail={img}
-          thumbnail={bun02}
-        />
-      </li>
-      <li className={burgerConstructorStyles.item}>
-        <img src={icon} alt='иконка меню' className={`${burgerConstructorStyles.icon} mr-2`}/>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          //thumbnail={img}
-          thumbnail={bun02}
-        />
-      </li>
-      <li className={burgerConstructorStyles.item}>
-        <img src={icon} alt='иконка меню' className={`${burgerConstructorStyles.icon} mr-2`}/>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          //thumbnail={img}
-          thumbnail={bun02}
-        />
-      </li>
-
+      {data.map(item => (
+        <ConstructorItem key={item._id} cardData={item}/>
+        ))}
     </ul>
   );
 }
@@ -63,10 +48,10 @@ const Order = () => {
 
 const BurgerConstructor = () => {
   return(
-    <>
+    <section className={`${burgerConstructorStyles.main} mt-25`}>
       <ConstructorItems />
       <Order />
-    </>
+    </section>
   );
 }
 
