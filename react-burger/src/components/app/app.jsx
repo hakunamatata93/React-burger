8a536724e618e35fe7a8eab757ea4c75f36e4ede
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-07-10 14:03:30 +0300

    fetch

--- react-burger/src/components/app/app.jsx
+++ react-burger/src/components/app/app.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import BurgerIngridients from "../burger-ingridients/burger-ingridients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import AppHeader from "../app-header/app-header";
@@ -5,7 +6,26 @@ import AppHeader from "../app-header/app-header";
 
 import appStyles from './app.module.css';
 
+const baseUrl = 'https://norma.nomoreparties.space/api/ingredients';
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json()
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
 const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://norma.nomoreparties.space/api/ingredients')
+    .then(checkResponse)
+    .then((data) => {
+      console.log(data);
+    })
+  }, [])
+  
   return(
     <div className={appStyles.app}>
       <AppHeader />
