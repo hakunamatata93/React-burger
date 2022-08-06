c6f8eba8a3178c5f0535570236990e81f627d860
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-08-06 13:32:27 +0300

    structure

--- react-burger/src/index.js
+++ react-burger/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { createStore, compose, applyMiddleware } from 'redux';
 import App from './components/app/app'
 import './index.css'
+import thunk from 'redux-thunk';
+import { rootReducer } from './services/reducers/rootReducer';
+import { Provider } from 'react-redux';
 
+// Redux DevTools 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose; 
 
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer)
 
 const root = ReactDOM.createRoot( document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <Provider store={store}>
+  <App />
+</Provider>
   );
