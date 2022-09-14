093692002a06b7353d95effcd03fa13b6688cee6
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-09-14 12:22:16 +0300

    bug fix

--- react-burger/src/components/modal/modal.jsx
+++ react-burger/src/components/modal/modal.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
+
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import modalStyles from './modal.module.css';
 import ModalOverlay from '../modal-overlay/modal-overlay';
+
+import modalStyles from './modal.module.css';
 
 
 const modalRoot = document.getElementById('react-modals');
@@ -30,9 +32,9 @@ const Modal = (props) => {
         {props.showModal && <ModalOverlay closing={props.closing} />}
         <div className={modalStyles.modal}>
 
-        <div onClick={props.closing} className={modalStyles.button}>
+        {props.showModal && <div onClick={props.closing} className={modalStyles.button}>
             <CloseIcon />
-          </div>
+          </div>}
           
           {props.children}
 
