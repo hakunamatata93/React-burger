8d27b87534f325d4d8d3476a60298c18cf86b4ae
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-09-16 17:21:57 +0300

    order card

--- react-burger/src/components/orders-feed/orders-feed.jsx
+++ react-burger/src/components/orders-feed/orders-feed.jsx
@@ -1,44 +1,48 @@
 import { useSelector } from 'react-redux';
-
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-
 import ordersFeedStyles from './orders-feed.module.css';
 
-const icons = [
-  "https://code.s3.yandex.net/react/code/bun-02-mobile.png", 
-  "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-  "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
-  "https://code.s3.yandex.net/react/code/meat-02-mobile.png"
-  ]
+
 
 export const CardOrder = ({card}) => {
 
+  const { name, number, createdAt, ingredients: ingridientsId} = card;
+
   const { orders } = useSelector(store => store.ws);
-  const { ingridients } = useSelector(store => store.ingridients)
+  const { ingridients } = useSelector(store => store.ingridients);
 
-
-
+  const orderedIngridients = ingridientsId.map(item => {
+    return ingridients.find(el => el._id === item);
+  });
 
   return (
     <article className={ordersFeedStyles.card}>
 
       <div className={ordersFeedStyles.info}>
-        <p className="text text_type_digits-default">#{card.number}</p>
+        <p className="text text_type_digits-default">#{number}</p>
         <p className="text text_type_main-default text_color_inactive">Сегодня, 16:20 i-GMT+3</p>
       </div>
               
-      <p className="text text_type_main-medium  mt-6 mb-6">{card.name}</p>
+      <p className="text text_type_main-medium  mt-6 mb-6">{name}</p>
 
       <div className={ordersFeedStyles.total}>
+
         <ul className={ordersFeedStyles.icons}>
-          <li className={ordersFeedStyles.img}>
-            <img src={icons[0]} className={ordersFeedStyles.icon}/>
-          </li>
-        
-          
-          <div className={ordersFeedStyles.icon} style={{backgroundImage: `url(${icons[2]})` }}>
-            <p className={`${ordersFeedStyles.lastIcon} text text_type_main-default`}>+3</p>
-          </div>
+          { 
+            (ingridientsId.length > 5) &&
+              ( <div className={ordersFeedStyles.icon} style={{backgroundImage: `url(${orderedIngridients[5].image_mobile})` }}>
+                  <p className={`${ordersFeedStyles.lastIcon} text text_type_main-default`}>+{orderedIngridients.length - 5}</p>
+                </div>)
+          }
+          {
+            orderedIngridients.slice(0, 5).reverse().map(item => {
+              return (
+                <li className={ordersFeedStyles.img}>
+                  <img src={item.image_mobile} className={ordersFeedStyles.icon}/>
+                </li>
+              )
+            })
+          }
         </ul>
 
         <div className={`${ordersFeedStyles.price} ml-6`}>
@@ -68,6 +72,5 @@ export const OrdersFeed = () => {
         }
       </ul>
     </section>
-  
   );
 };
