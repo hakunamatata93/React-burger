918d53e739c396087f125d70836f98f96f53ff8e
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-09-16 16:35:31 +0300

    ws

--- react-burger/src/utils/constants.js
+++ react-burger/src/utils/constants.js
@@ -1,4 +1,7 @@
 export const BASEURL= 'https://norma.nomoreparties.space/api';
+
+export const wsUrl = 'https://norma.nomoreparties.space/orders';
+
 export function checkResponse(res) {
     if (res.ok) {
       return res.json()
