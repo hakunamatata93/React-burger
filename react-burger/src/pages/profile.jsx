a57e4c8bfe559a272dc8101ac69eddcf944d26ab
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-09-16 18:52:39 +0300

    profile orders

--- react-burger/src/pages/profile.jsx
+++ react-burger/src/pages/profile.jsx
@@ -1,23 +1,21 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Redirect } from 'react-router-dom';
-
-import { Input, EditIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import { SET_UPDATE_USER, CANCEL_UPDATE_USER, getUser, updateUser } from '../services/actions/auth';
 import { logout } from '../services/actions/auth';
 
 import styles from './style.module.css';
+import { ProfileForm } from "../components/profile-form/profile-form";
 
 
 export const ProfilePage = () => {
 
   const dispatch = useDispatch();
   const form = useSelector(store => store.user.form);
-  //const isAuth = localStorage.getItem('token');
   const [actionButtons, setActionButtons] = useState(false);
   const { isAuth } = useSelector(store => store.user);
-
 
   const onChange = (evt) => {
     dispatch({
@@ -43,19 +41,12 @@ export const ProfilePage = () => {
       type: CANCEL_UPDATE_USER 
     });
   };
-
   
   if (!isAuth) {
     return (
-      <Redirect
-        to={{
-          pathname: '/login',
-        }}
-      />
+      <Redirect to={{ pathname: '/login' }} />
     );
-  }
-  
-  
+  }  
 
   return (
     <main className={`${styles.main} mt-30`}>
@@ -79,52 +70,7 @@ export const ProfilePage = () => {
       </section>
 
       <section>
-        <form onSubmit={onSubmitForm} className={`${styles.form} mb-20`}>
-          <fieldset className={styles.fieldset}>
-            <Input
-              type={'text'}
-              placeholder={'Имя'}
-              onChange={onChange}
-              value={form.name}
-              name={'name'}
-              icon={'EditIcon'}
-              errorText={"Ошибка"}          
-            />
-            <Input
-              type={'email'}
-              placeholder={'Логин'}
-              onChange={onChange}
-              value={form.email}
-              name={'email'}
-              icon={'EditIcon'}
-              errorText={"Ошибка"}
-              //onIconClick={console.log('кликнули на Логин')}
-              //disabled
-            />
-            <Input
-              type={'password'}
-              placeholder={'Пароль'}
-              onChange={onChange}
-              value={form.password || ''}
-              name={'password'}
-              icon={'EditIcon'}
-              errorText={"Ошибка"}
-              //onIconClick={console.log('кликнули на Пароль')}
-              //disabled
-            />
-          </fieldset>
-
-          {actionButtons && (<div className={styles.actions}>
-            <Button onClick={cancelUpdateForm} type="secondary" size="medium">
-              Отмена
-            </Button>
-
-            <Button type="primary" size="large">
-              Сохранить
-            </Button>
-          </div>)}
-
-        </form>
+        <ProfileForm />
       </section>
     </main>
   );
