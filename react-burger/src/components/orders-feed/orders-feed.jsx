397dcd661958a1b952212291fd121525672725a3
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-09-16 17:42:51 +0300

    card order component

--- react-burger/src/components/orders-feed/orders-feed.jsx
+++ react-burger/src/components/orders-feed/orders-feed.jsx
@@ -1,64 +1,9 @@
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CardOrder } from '../card-order/card-order';
 
 import ordersFeedStyles from './orders-feed.module.css';
-
-
-export const CardOrder = ({ card }) => {
-
-  const { name, number, createdAt, ingredients: ingridientsId} = card;
-
-  const { orders } = useSelector(store => store.ws);
-  const { ingridients } = useSelector(store => store.ingridients);
-
-  const orderedIngridients = ingridientsId.map(item => {
-    return ingridients.find(el => el._id === item);
-  });
-
-  const sumTotal = useMemo(() => {
-    return (
-      orderedIngridients.reduce((acc, item) => acc + item.price, 0)
-    );
-  }, [orderedIngridients]);
-
-  return (
-    <article className={`${ordersFeedStyles.card} mr-2`}>
-      <div className={ordersFeedStyles.info}>
-        <p className="text text_type_digits-default">#{number}</p>
-        <p className="text text_type_main-default text_color_inactive">Сегодня, 16:20 i-GMT+3</p>
-      </div>
-              
-      <p className="text text_type_main-medium  mt-6 mb-6">{name}</p>
-
-      <div className={ordersFeedStyles.total}>
-        <ul className={ordersFeedStyles.icons}>
-          { 
-            (ingridientsId.length > 5) &&
-              ( <div className={ordersFeedStyles.icon} style={{backgroundImage: `url(${orderedIngridients[5].image_mobile})` }}>
-                  <p className={`${ordersFeedStyles.lastIcon} text text_type_main-default`}>+{orderedIngridients.length - 5}</p>
-                </div>)
-          }
-          {
-            orderedIngridients.slice(0, 5).reverse().map((item, index) => {
-              return (
-                <li key={index} className={ordersFeedStyles.img}>
-                  <img src={item.image_mobile} className={ordersFeedStyles.icon}/>
-                </li>
-              )
-            })
-          }
-        </ul>
-
-        <div className={`${ordersFeedStyles.price} ml-6`}>
-          <p className="text text_type_digits-default mr-2">{sumTotal}</p>
-          <CurrencyIcon type="primary" />
-        </div>
-      </div>
-    </article>
-  )
-}
 
 
 export const OrdersFeed = () => {
@@ -68,12 +13,11 @@ export const OrdersFeed = () => {
   return (
     <section>
       <ul className={`${ordersFeedStyles.cardList} custom-scroll`}>
-      {orders.map(item => (
-            <li key={item._id}>
-              <CardOrder card={item}/>
-              </li>
-          ))
-        }
+        {orders.map(item => (
+            
+          <CardOrder card={item} key={item._id}/>
+            
+        ))}
       </ul>
     </section>
   );
