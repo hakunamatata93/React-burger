f572e4e69729a1e89569dd589f57dd1af99d3f6c
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-10-16 00:15:14 +0300

    warning fix

--- react-burger/src/index.tsx
+++ react-burger/src/index.tsx
@@ -1,15 +1,19 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { store } from './services/store';
-import { App } from './components/app/app';
+
+import { store } from '../src/services/store';
+import {App} from './components/app/app';
 
 import './index.css';
 
-ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>, 
-  document.getElementById('root'));
+  </Router>
+  );
