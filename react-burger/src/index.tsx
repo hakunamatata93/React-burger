77bd2fd0ed758ea08206ee3b98b19b2d7295be3a
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-10-15 15:04:45 +0300

    index

--- react-burger/src/index.tsx
+++ react-burger/src/index.tsx
@@ -2,12 +2,14 @@ import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { store } from '../src/services/store';
+import { store } from './services/store';
 import App from './components/app/app';
 
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <Router>
     <Provider store={store}>
